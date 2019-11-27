cc28a072117ad39d5cfbda12f80d96e6b590359d
Dima Statz <dima.statz@gmail.com>
2019-11-27 12:19:23 +0200

    Create run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+# clear all
+rm -rf /tmp/data-collector
+
+# nginx conf
+mkdir /tmp/data-collector
+mkdir /tmp/data-collector/nginx
+touch /tmp/data-collector/nginx/access.log
+touch /tmp/data-collector/nginx/error.log
+
+# fluentd
+mkdir /tmp/data-collector/fluentd
+mkdir /tmp/data-collector/fluentd/td-agent
+touch /tmp/data-collector/fluentd/td-agent/access.log.pos
+chmod 666 /tmp/data-collector/fluentd/td-agent/access.log.pos
+
+# run docker-compose
+docker-compose up --force-recreate --build
